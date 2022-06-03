7ee0c8b9360d63e372411c7c85c648a7bc05a184
Rajiv Verma <rajiv@webguy.in>
2022-06-03 20:34:02 +0530

    Basic UI & routing complete

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::post('upload', function (Request $request) {
+    dd('here in post');
+})->name('upload.image');
